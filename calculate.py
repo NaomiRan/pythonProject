df7be02cc1d0e9d7eb66adb31186124899fbba6f
NaomiRan <naomiran1989@gmail.com>
2024-03-31 22:56:39 -0400

    practice input and output function

---
+++ calculate.py
@@ -0,0 +1,22 @@
+# num1=int(input("Number 1= "))
+# num2=int(input("Number 2= "))
+# if num1*num2 <1000:
+#     print(f'The result is {num1*num2}')
+# else:
+#     print(f'The result is {num1+num2}')
+
+# # 2 loop
+# print('Printing current and previous number sum in a range(10)')
+# for i in range(0,10):
+#     if i <1:
+#         print(f'Current Number 0  Previous Number 0    Sum: 0')
+#     else:
+#       print(f'Current Number {i}  Previous Number {i-1}    Sum: {i+i-1}')
+
+#Q3 print even index character
+str=input('Orginal String is  ')
+print('Printing only even index chars')
+
+for n in range(0,len(str)-1,2):
+        print(str[n])
+
--- maxNum.py
+++
@@ -1,6 +0,0 @@
-numbers=[12,2]
-maxNum=numbers[0]
-for i in numbers:
-    if(i>maxNum):
-        maxNum=i
-print(f"the larger number is {maxNum}")
